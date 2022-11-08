936daa790e10848a2595e4b1d2e76c291db01cc9
ryt31 <ryota.game10@gmail.com>
2022-11-08 22:02:36 +0900

    お知らせのURLフィールドを情報なしでも許容するようにした

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class Information(models.Model):
     date = models.DateField(verbose_name='日時')
-    url = models.URLField(verbose_name='URL', null=True, unique=False)
+    url = models.URLField(verbose_name='URL', null=True, blank=True, unique=False)
     content = models.TextField(verbose_name='本文')
     created_at = models.DateField(verbose_name='作成日時', auto_now_add=True)
