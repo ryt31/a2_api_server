6954249708c8420a91f8070c40643d97158bbd1a
ryt31 <ryt.a2lab@gmail.com>
2022-11-08 16:02:53 +0000

    PythonAnyWhere auto write.

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
+MEDIA_ROOT = Path(BASE_DIR / 'media')
